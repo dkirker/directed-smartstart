1005b812bb25599f11437f9471ccba07b38a20d2
Donald Kirker <donald.kirker@openmobl.com>
2013-06-25 01:29:53 -0700

    Add SmartStart API.

--- source/App.js
+++ source/App.js
@@ -3,15 +3,80 @@ enyo.kind({
 	kind: "FittableRows",
 	fit: true,
 	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
-		{kind: "enyo.Scroller", fit: true, components: [
-			{name: "main", classes: "nice-padding", allowHtml: true}
+        {kind: "directed.SmartStartAPI", name: "api",
+            onLoggedIn: "handleLoggedIn", onLogInError: "handleLogInError",
+            onVehiclesReslt: "handleVehicleResult", onVehiclesError: "handleVehiclesError",
+            onCommandResult: "handleCommandResult", onCommandError: "handleCommandError"
+        },
+        
+		{kind: "onyx.Toolbar", content: "SmartStart"},
+		{kind: "enyo.Scroller", name: "logInScroller", fit: true, components: [
+            {kind: "FittableColumns", components: [
+                {kind: "onyx.InputDecorator", components: [
+                    {kind: "onyx.Input", name: "username", placeholder: "Username", onchange: "inputChange"}
+                ]}
+            ]},
+            {kind: "FittableColumns", components: [
+                {kind: "onyx.InputDecorator", components: [
+                    {kind: "onyx.Input", name: "password", placeholder: "Password", onchange: "inputChange"}
+                ]}
+            ]},
+            {kind: "FittableColumns", components: [
+                {kind: "onyx.Button", content: "Log In", ontap: "logIn"}
+            ]}
+		]},
+		{kind: "enyo.Scroller", name: "commandScroller", fit: true, components: [
+            
 		]},
 		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
+			{kind: "onyx.Button", content: "Vehicles", ontap: "selectVehicles"}
 		]}
 	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.addContent("The button was tapped.<br/>");
-	}
+    
+    sessionId: "",
+    vehicles: [],
+    
+    constructor: function() {
+        this.inherited(arguments);
+    },
+    create: function() {
+        this.inherited(arguments);
+        
+        if (enyo.platform.webos) {
+			PalmSystem.stageReady();
+		}
+    },
+    
+    // Instigate the API
+    inputChange: function(inSender, inEvent) {
+    
+    },
+    
+    logIn: function(inSender, inEvent) {
+    
+    },
+    
+	selectVehicles: function(inSender, inEvent) {
+		
+	},
+    
+    // Handle API results
+    handleLoggedIn: function(sessionData) {
+        if (sessionData) {
+            this.sessionId = sessionData.sessionId;
+        }
+    },
+    handleLogInError: function(error) {
+    },
+    handleVehicleResult: function(vehicles, inSender, inResponse) {
+        this.vehicles = vehicles;
+    },
+    handleVehiclesError: function(error) {
+    },
+    handleCommandResult: function(inSender, inResponse) {
+    },
+    handleCommandError: function(error) {
+    },
+    
+    // Send commands
 });
---
+++ source/SmartStartAPI.js
@@ -0,0 +1,179 @@
+enyo.kind({
+	name: "directed.SmartStartAPI",
+	kind: "enyo.Control",
+	components:[
+		
+	],
+    
+    events: {
+        onLoggedIn: "",
+        onLogInError: "",
+        onVehiclesReslt: "",
+        onVehiclesError: "",
+        onCommandResult: "",
+        onCommandError: ""
+    },
+    
+    host: "colt.calamp-ts.com",
+    username: "",
+    password: "",
+    sessionId: "",
+    
+    devices: [],
+    
+    /**
+     * Force reset of session ID and log in.
+     */
+    logIn: function(username, password) {
+        this.username = username;
+        this.password = password;
+        
+        this.getSessionId();
+    },
+    
+    /**
+     * Get session ID with given credentials.
+     */
+    getSessionId: function() {
+        var loginUrl = "https://" + this.host + "/auth/login/" + this.username + "/" + this.password;
+        
+        var request = new enyo.Ajax({
+            url: loginUrl,
+            handleAs: "json",
+            contentType: "text/plain"
+        });
+
+        request.response(enyo.bind(this, "processLogin"));
+        request.go();
+    },
+    
+    /**
+     * Set session ID from previously stored session.
+     */
+    setSessionId: function(newSessionId) {
+        this.sessionId = newSessionId;
+    },
+    
+    processLogin: function(inSender, inResponse) {
+        /*if (!inSender || !inSender.xhrResponse) {
+            this.doLogInError({"errorCode": , "errorText": "Invalid response"});
+            return;
+        }*/
+        if (!inResponse || !inResponse.Results) {
+            this.sessionId = "";
+            this.doLogInError({"errorCode": "", "errorText": "Invalid response"});
+            return;
+        }
+        
+        /*var headers = inSender.xhrResponse.headers;
+        var setCookie = headers["set-cookie"];
+        
+        if (setCookie) {
+            var exp = new RegExp("^kohanasession=(.*?);", "m");
+            
+            // TODO: Just read inResponse.Results.SessionID
+            this.sessionId = exp.exec(setCookie);
+        }*/
+        
+        var sessionID = inResponse.Results.SessionID;
+        if (sessionID) {
+            this.sessionId = sessionID;
+            this.doLoggedIn({"sessionId": this.sessionId});
+        } else {
+            // We need to error out here!
+            this.sessionId = "";
+            this.doCommandError({"errorCode": "", "errorText": "No session ID"});
+        }
+    },
+    
+    getVehicles: function() {
+        if (!this.sessionId || this.sessionId == "") {
+            this.doVehiclesError({"errorCode": "", "errorText": "No session ID"});
+            return;
+        }
+        
+        var vehiclesUrl = "https://" + this.host + "/device/advancedsearch?sessid=" + this.sessionId;
+        var request = new enyo.Ajax({
+            url: vehiclesUrl,
+            handleAs: "json",
+            contentType: "text/plain"
+        });
+
+        request.response(enyo.bind(this, "processVehicles"));
+        request.go();
+    },
+    
+    processVehicles: function(inSender, inResponse) {
+        if (!inResponse || !inResponse.Results || !inResponse.Results.Devices) {
+            this.doVehiclesError({"errorCode": "", "errorText": "Invalid response"});
+            return;
+        }
+        
+        this.devices = inResponse.Results.Devices;
+        
+        // Return results:
+        // deviceId (DeviceId)
+        // name (Name)
+        // description (Description)
+        // vin (Field1)
+        // modelYear (Field4)
+        // make (Filed5)
+        // model (field6)
+        // airId (AirId)
+        // commands (AvailActions)
+        //     Name
+        //     Description
+        var map = [
+                {"from": "DeviceId", "to": "deviceId"},
+                {"from": "Name", "to": "name"},
+                {"from": "Description", "to": "description"},
+                {"from": "Field1", "to": "vin"},
+                {"from": "Field5", "to": "make"},
+                {"from": "Field6", "to": "model"},
+                {"from": "AirId", "to": "airId"}
+            ];
+        var vehicles = [];
+        for (var i = 0; i < this.devices.length; i++) {
+            for (var j = 0; j < map.length; j++) {
+                vehicles[i][map[j].to] = this.devices[i][map[j].from];
+            }
+            /*vehicles[i].commands = [];
+            for (var k = 0; k < this.devices[i].AvailActions; k++) {
+                vehicles[i].commands[k] = this.devices[i].AvailActions[k];
+            }*/
+            vehicles[i].commands = this.devices[i].AvailActions;
+        }
+        
+        this.doVehiclesResult(vehicles, inSender, inResponse);
+    },
+    
+    sendCommand: function(vehicleId, command) {
+        if (!this.sessionId || this.sessionId == "") {
+            this.doCommandError({"errorCode": "", "errorText": "No session ID"});
+            return;
+        }
+        if (!vehicleId || !command) {
+            this.doCommandError({"errorCode": "", "errorText": "Invalid argument"});
+            return;
+        }
+        
+        var commandUrl = "https://" + this.host + "/device/sendcommand/" + vehicleId + "/" + command + "?sessid=" + this.sessionId;
+        var request = new enyo.Ajax({
+            url: commandUrl,
+            handleAs: "json",
+            contentType: "text/plain"
+        });
+
+        request.response(enyo.bind(this, "processCommandResult"));
+        request.go();
+    },
+    
+    processCommandResult: function(inSender, inResponse) {
+        if (!inResponse) {
+            this.doCommandError({"errorCode": "", "errorText": "Invalid response"});
+            return;
+        }
+        this.doCommandResult(inSender, inResponse);
+    },
+    
+});
